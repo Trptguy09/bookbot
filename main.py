0da0f4651fa3ba7ed7bbdd9b6666b574d96e8a6e
Trptguy09 <nervouspuddin@gmail.com>
2025-09-01 15:45:17 -0400

    create report

--- main.py
+++ main.py
@@ -7,12 +7,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_counts = get_characters_dict(text)
-    print (f"{num_words} words found in the document")
-    print (char_counts)
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
+def print_report()
+    
 main() 
      
