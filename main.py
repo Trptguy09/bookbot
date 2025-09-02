efbdc460baeabd932ed4c616fcd8fc94587f128e
Trptguy09 <nervouspuddin@gmail.com>
2025-09-01 20:29:18 -0400

    working character sort

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from stats import get_num_words
-from stats import get_characters_dict
+from stats import (get_num_words,
+                   get_characters_dict,
+                   chars_dict_to_sorted_list)
+
 
 
 def main():
     book_path ="books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_counts = get_characters_dict(text)
+    chars_dict = get_characters_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list) 
 
-def print_report()
-    
-main() 
-     
+def get_book_text(path):
+     with open(path) as f:
+          return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print ("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
+
+main()
--- stats.py
+++ stats.py
@@ -2,9 +2,25 @@ def get_num_words(text):
     words = text.split()
     return len (words)
 
+
 def get_characters_dict(text):
-    characters = text.lower()
-    char = {}
-    for character in characters:
-       char[character] = char.get(character,0) + 1
-    return char
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+           chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char" : ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
